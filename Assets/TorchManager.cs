e5a752a9ac15702986f3ddf1484081b56e8ff630
lukasmakegames <lugiro43@gmail.com>
2023-05-22 17:34:25 +0200

    add torchs manager

---
+++ Assets/TorchManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchManager : MonoBehaviour
+{
+
+    public GameObject[] Torchs;
+
+  private int currentTorch = 0;
+
+  // Start is called before the first frame update
+  void Start()
+    {
+    if (Torchs.Length == 3)
+    {
+      if (Torchs[0] != null && !Torchs[0].active)
+        Torchs[0].SetActive(true);
+      if (Torchs[1] != null && Torchs[1].active)
+        Torchs[1].SetActive(false);
+      if (Torchs[2] != null && Torchs[2].active)
+        Torchs[2].SetActive(false);
+
+    }
+  }
+
+  // Update is called once per frame
+  void Update()
+    {
+      if (Input.GetButtonDown("Fire1"))
+      {
+        Torchs[currentTorch].SetActive(false);
+      var pos = Torchs[currentTorch].transform.position;
+        currentTorch++;
+        if (currentTorch > 2)
+          currentTorch = 0;
+      Torchs[currentTorch].transform.position = pos;
+      Torchs[currentTorch].SetActive(true);
+        
+      }
+    }
+}
