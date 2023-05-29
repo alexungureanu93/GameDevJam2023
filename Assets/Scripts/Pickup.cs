6c7b6ea2c3e3b4943467f2f621aa0f1790693161
Ungureanu Alexandru <alex.ungureanu16@yahoo.ro>
2023-05-29 17:50:03 +0300

    Fix conflicts on Pickup script and coin prefab

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
-<<<<<<< Updated upstream
-    void OnTriggerEnter2D(Collider2D other) 
-=======
-  public GameObject ExplosionPrefab;
+    public GameObject ExplosionPrefab;
     public AudioClip destroySound;
 
   void OnTriggerEnter2D(Collider2D other) 
->>>>>>> Stashed changes
     {
         //Add the coin to the player wallet when collectd
         Wallet wallet = other.GetComponent<Wallet>();
