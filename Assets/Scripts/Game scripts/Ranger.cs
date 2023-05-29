c3ea0dcaee9e74ef4dec9f8219b6eac69288a8d1
Ungureanu Alexandru <alex.ungureanu16@yahoo.ro>
2023-05-29 20:22:54 +0300

    Commit Ranger initial script and bullet prefab

---
+++ Assets/Scripts/Game scripts/Ranger.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ranger : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletPos;
+
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > 1) 
+        {
+            timer = 0;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(bullet,bulletPos.position,Quaternion.identity);
+    }
+}
